12fdb704cd22ffb2a486288576a5b347db105f5d
Contrigra <gaponov60b@gmail.com>
2026-07-22 16:51:12 +0300

    feat(footer): added github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,5 +2,7 @@ import {JSX} from "react";
 
 export default function Footer(): JSX.Element {
     // TODO Footer
-    return <div className={"footer"}>Footer placeholder</div>
+    return <div className={"footer"}>
+        <a href="https://github.com/Contrigra/danganronpa_memory_game">Github Page</a>
+    </div>
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import {JSX} from "react";
 export default function Header({title}: { title: string }): JSX.Element {
     return (
         <div className={"header"}>
-            {/*<h1 className={"title"}>{title}</h1>*/}
             <div className={"title"}><img src="/src/assets/header_image.png" className={"headerImage"}/></div>
             <div className="current-score">Current Score: 3</div>
             <div className="high-score">High Score: 11</div>
