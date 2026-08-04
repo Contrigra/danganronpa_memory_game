f897da7134d8dc5c19cd8bc0ba6b14c78ccfe71a
Contrigra <gaponov60b@gmail.com>
2026-08-04 12:45:40 +0300

    fix(deploy): fix header image import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import {JSX} from "react";
+import headerImage from '../assets/header_image.png'
+
+
+
 
 // TODO dynamic score
 export default function Header({currentScore, highScore}: { currentScore: number, highScore: number }): JSX.Element {
     return (
         <div className={"header"}>
-            <div className={"title"}><img src="/src/assets/header_image.png" className={"headerImage"}/></div>
+            <div className={"title"}><img src={`${headerImage}`} className={"headerImage"}/></div>
             <div className="current-score">Current Score: {currentScore}</div>
             <div className="high-score">High Score: {highScore}</div>
         </div>
